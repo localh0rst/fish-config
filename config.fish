641abbf84750b16a1e0402c7fdca610b5aed7fe4
localh0rst <git@fail.ninja>
2024-05-05 10:46:27 +0200

    Moved documentation to readme

--- config.fish
+++ config.fish
@@ -7,31 +7,26 @@ for file in ~/.config/fish/completions/*.fish
     source $file
 end
 
-#if status is-interactive
+
+if status is-interactive
+    # Set up perlbrew (remember to run perlbrew init, perlbrew install-cpanm, perlbrew install perl-5.38.2, perlbrew switch perl-5.38.2)
+    # source perlbrew.fish if it exists
+    test -e ~/perl5/perlbrew/etc/perlbrew.fish; and source ~/perl5/perlbrew/etc/perlbrew.fish
+
+    # Setup aliases, env vars, and path
+    source ~/.config/fish/aliases.fish
+    source ~/.config/fish/env.fish
+    source ~/.config/fish/path.fish
+
+    starship init fish | source
+
+    # UIse default key bindings
+    fish_default_key_bindings
+
 #    if not set -q TMUX
 #        exec tmux new-session -A -c $HOME -s BASIC
 #    end
-#end
+end
 
-source ~/.config/fish/aliases.fish
-source ~/.config/fish/env.fish
-source ~/.config/fish/path.fish
 
-# Install fisher (curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
-# Fuzzy finder
-# fisher install PatrickF1/fzf.fish
-#   CTRL-ALT-F search Files
-#   CTRL-ALT-L search Gitlog
-#   CTRL-ALT-S search Git status
-#   CTRL-R search history
-#   CTRL-ALT-P search process
-#   CTRL-V search variables
-#
-# fisher install jorgebucaran/fish-ssh-agent
 
-# Install starship (https://starship.rs/)
-# curl -sS https://starship.rs/install.sh | sh
-starship init fish | source
-
-fish_default_key_bindings
-#bind \cA beginning-of-line
