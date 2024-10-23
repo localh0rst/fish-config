e900b327ca43f0875f88a0d5f2df392a60e2579e
Sebastian Grenz <git@fail.ninja>
2024-10-23 14:37:11 +0200

    added additional paths

--- path.fish
+++ path.fish
@@ -1,3 +1,9 @@
 fish_add_path -p $HOME/.local/bin
+
+fish_add_path -p $HOME/.local/bin/scripts/backup/
+fish_add_path -p $HOME/.local/bin/scripts/random/
+fish_add_path -p $HOME/.local/bin/scripts/tmux/
+fish_add_path -p $HOME/.local/bin/scripts/window_manager/
+
 fish_add_path -p $HOME/perl5/perlbrew/bin
 
